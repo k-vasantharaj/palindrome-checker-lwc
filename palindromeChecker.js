90f74c0f24222339ba269aaa6da5642c32033d1c
Vasantharaj <vr1431516@gmail.com>
2026-02-25 02:50:29 +0530

    Update palindromeChecker.js

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -1,21 +1,38 @@
 import { LightningElement } from 'lwc';
 
-export default class PalindromeChecker extends LightningElement {
-    word = '';     
-    reversedWord = '';  
-    isPalindrome = '';
-
-    handleWordChange(event) {
+export default class LwcBinding extends LightningElement {
+word;     
+reversedWord;  
+isPalindrome;
+result;
+handleWordChange(event) 
+{
         this.word = event.target.value;
-        this.isPalindrome = ''; // clear old result
-    }
-
-    handleCheckPalindrome() {
-        this.reversedWord = this.word.split('').reverse().join('');
-        if (this.word === this.reversedWord) {
-            this.isPalindrome = '✅ Word is a palindrome';
-        } else {
-            this.isPalindrome = '❌ Word is not a palindrome';
-        }
-    }
 }
+
+handleCheckPalindrome() 
+{
+        if (this.word ==='') 
+        {
+            this.isPalindrome = '';
+            this.result = '';
+            return;
+        }
+        this.reversedWord = this.word.split('').reverse().join('');
+        if(this.word === this.reversedWord)
+        {
+                this.isPalindrome= ' Word is a palindrome';
+                this.result='palindrome';
+        }
+        else  
+        {
+                this.isPalindrome= ' Word is not a palindrome';
+                this.result='not-palindrome';
+        }
+        
+
+}
+
+
+}
+       
